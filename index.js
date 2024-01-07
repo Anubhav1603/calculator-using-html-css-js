7dacae84c53223aac0833d506b370777b5cf6c42
Anubhav1603 <anubhavsinha110@gmail.com>
2024-01-07 22:42:26 +0530

    reduced the character limit on screen due to overflow

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ document.querySelectorAll("button").forEach(function (e) {
         if (typeof num == "undefined") {
           screenText.innerText = 0;
         } else {
-          screenText.innerText = String(num).slice(0, 18);
+          screenText.innerText = String(num).slice(0, 17);
           lastEvaluationError = false;
         }
       } catch (error) {
         lastEvaluationError = true;
       }
     } else {
-      if (screenText.innerText.length < 18) {
+      if (screenText.innerText.length < 17) {
         screenText.innerText += event.target.textContent;
       } else {
-        alert("Character limit reached (max 18 characters).");
+        alert("Character limit reached (max 17 characters).");
       }
     }
     console.log(event.target.textContent);
